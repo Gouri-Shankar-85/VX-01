f7f21390f8d556adfaad43279063d8a33786aa6c
Gouri-Shankar-85 <sskzm5585@gmail.com>
2026-03-08 15:09:30 +0530

    test

--- vx01_ws/src/vx01_control/locomotion/vx01_hexapod_locomotion/src/hexapod_locomotion.cpp
+++ vx01_ws/src/vx01_control/locomotion/vx01_hexapod_locomotion/src/hexapod_locomotion.cpp
@@ -127,11 +127,8 @@ namespace vx01_hexapod_locomotion {
         double gait_x, gait_y, gait_z;
         gait_pattern_->getFootPosition(leg_index, t, gait_x, gait_y, gait_z);
 
-        const double alpha  = leg_angles_[leg_index];
-        const double stride = gait_y;
-
-        const double ik_x = home_x_ + std::cos(alpha) * stride;
-        const double ik_y =         - std::sin(alpha) * stride;
+        const double ik_x = home_x_ * gait_y;
+        const double ik_y = gait_y;
         const double ik_z = home_z_ + gait_z;
 
         applyIK(leg_index, ik_x, ik_y, ik_z);
