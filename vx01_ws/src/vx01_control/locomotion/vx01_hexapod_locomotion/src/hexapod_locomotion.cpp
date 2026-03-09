7a89e4fabee052a63321b241c82dc9dfb5d0ef21
Gouri-Shankar-85 <sskzm5585@gmail.com>
2026-03-09 13:47:49 +0530

    fix: project stride offset into leg-local frame in updateLeg, tune yaml for IK reachability

--- vx01_ws/src/vx01_control/locomotion/vx01_hexapod_locomotion/src/hexapod_locomotion.cpp
+++ vx01_ws/src/vx01_control/locomotion/vx01_hexapod_locomotion/src/hexapod_locomotion.cpp
@@ -127,7 +127,11 @@ namespace vx01_hexapod_locomotion {
         double gait_x, gait_y, gait_z;
         gait_pattern_->getFootPosition(leg_index, t, gait_x, gait_y, gait_z);
 
-        applyIK(leg_index, gait_x, gait_y, home_z_ + gait_z);
+        const double angle = leg_angles_[leg_index];
+        const double local_x = gait_x + std::sin(angle) * gait_y;
+        const double local_y =          std::cos(angle) * gait_y;
+
+        applyIK(leg_index, local_x, local_y, home_z_ + gait_z);
     }
 
     std::vector<double> HexapodLocomotion::getJointAngles() const {
