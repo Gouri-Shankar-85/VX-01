c3ecaec836e724aef60c599bc8d6bcac2a78eb70
Gouri-Shankar-85 <sskzm5585@gmail.com>
2026-03-26 17:19:57 +0530

    fix: added a use sim time argument to forward walk launch file

--- vx01_ws/src/vx01_control/locomotion/vx01_locomotion_control/launch/hexapod_forward_walk.launch.py
+++ vx01_ws/src/vx01_control/locomotion/vx01_locomotion_control/launch/hexapod_forward_walk.launch.py
@@ -2,6 +2,8 @@
 
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
@@ -13,12 +15,21 @@ def generate_launch_description():
         'config', 'hexapod_locomotion.yaml'
     )
 
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Set to true when running in Gazebo simulation'
+    )
+
     node = Node(
         package='vx01_locomotion_control',
         executable='hexapod_forward_walk_node',
         name='hexapod_forward_walk_node',
-        parameters=[config, {'use_sim_time': False}],
+        parameters=[config, {'use_sim_time': LaunchConfiguration('use_sim_time')}],
         output='screen',
     )
 
-    return LaunchDescription([node])
+    return LaunchDescription([
+        use_sim_time_arg,
+        node,
+    ])
