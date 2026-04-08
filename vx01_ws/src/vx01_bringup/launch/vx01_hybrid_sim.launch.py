a70e421864328b688c6bc7b09de5e5effda62c2e
Gouri-Shankar-85 <sskzm5585@gmail.com>
2026-04-08 09:13:44 +0530

    feat: add rosbridge_server to launch configuration and fix ArduPilotPlugin library path in URDF

--- vx01_ws/src/vx01_bringup/launch/vx01_hybrid_sim.launch.py
+++ vx01_ws/src/vx01_bringup/launch/vx01_hybrid_sim.launch.py
@@ -67,6 +67,13 @@ def generate_launch_description():
         output='screen'
     )
 
+    rosbridge_server = Node(
+        package='rosbridge_server',
+        executable='rosbridge_websocket',
+        output='screen',
+        parameters=[{'port': 9090}]
+    )
+
     web_video_server = Node(
         package='web_video_server',
         executable='web_video_server',
@@ -79,6 +86,6 @@ def generate_launch_description():
         sitl_layer,
         TimerAction(
         period=18.0,
-            actions=[mavros_node, mode_manager, aerial_controller, web_video_server]
+            actions=[mavros_node, mode_manager, aerial_controller, web_video_server, rosbridge_server]
         )
     ])
