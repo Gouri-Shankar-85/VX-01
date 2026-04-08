ce4f3c138637715dbeb2f24eeb353707c512b096
Gouri-Shankar-85 <sskzm5585@gmail.com>
2026-04-08 12:15:32 +0530

    chore: update ArduPilot serial port to 14551 and simplify MAVROS fcu_url configuration

--- vx01_ws/src/vx01_bringup/launch/vx01_hybrid_sim.launch.py
+++ vx01_ws/src/vx01_bringup/launch/vx01_hybrid_sim.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
             '/ardupilot/Tools/autotest/default_params/copter.parm,'
             '/ardupilot/Tools/autotest/default_params/gazebo-iris.parm,'
             '/vx01_ws/src/vx01_bringup/config/ardupilot_sim_bypass.parm',
-            '--serial1',  'out:udp:127.0.0.1:14550',
+            '--serial1',  'out:udp:127.0.0.1:14551',
         ],
         cwd='/ardupilot/ArduCopter',
         output='screen'
@@ -41,7 +41,7 @@ def generate_launch_description():
         output='screen',
         parameters=[
             # Bind to UDP 14550 and wait for incoming heartbeats from SITL.
-            {'fcu_url': 'udp://127.0.0.1:14551@14555'},
+            {'fcu_url': 'udp://127.0.0.1:14551@'},
             {'use_sim_time': True},
             os.path.join(get_package_share_directory('vx01_bringup'), 'config', 'mavros_sim_config.yaml'),
             {'gcs_url': ''},
